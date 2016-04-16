8075c465138bf12d44a4c8d618eef5f0b33b31b5
Clement-R <rondeau.clement@gmail.com>
2016-04-17 00:00:37 +0200

    What a hard jam ... motivation to the max until the end!

--- Game.js
+++ Game.js
@@ -27,6 +27,8 @@ BasicGame.Game = function (game) {
 
 BasicGame.Game.prototype = {
     create: function () {
+        this.stage.backgroundColor = "#2b87c4"
+
         // Set up game physic engine
         this.physics.startSystem(Phaser.Physics.ARCADE);
         this.physics.arcade.gravity.y = 350;
@@ -34,32 +36,18 @@ BasicGame.Game.prototype = {
         // Create map
         var map = this.add.tilemap("map");
         map.addTilesetImage("wall");
-        map.addTilesetImage("ladder");
 
         layer = map.createLayer("Calque de Tile 1");
-        layer_ladders = map.createLayer("ladders");
         outer = map.createLayer("outer");
+        house = map.createLayer("house");
 
         map.setCollisionBetween(1, 1, true, "Calque de Tile 1");
         map.setCollisionBetween(1, 1, true, "outer");
 
-        map.setCollisionBetween(2, true, "ladders");
-        map.setTileIndexCallback(2, function () {
-            if(cursors.up.isDown) {
-                player.body.velocity.y = -250;
-            }
-        }, this, layer_ladders);
-
         layer.resizeWorld();
 
         // Set up player
-        var player_texture = this.add.bitmapData(32, 32);
-        player_texture.ctx.beginPath();
-        player_texture.ctx.rect(0,0,32,32);
-        player_texture.ctx.fillStyle = '#0000ff';
-        player_texture.ctx.fill();
-
-        player = this.add.sprite(100, 100, player_texture);
+        player = this.add.sprite(33 * 32, 12 * 32, "player");
 
         this.physics.enable(player, Phaser.Physics.ARCADE);
         player.body.collideWorldBounds = true;
@@ -69,7 +57,9 @@ BasicGame.Game.prototype = {
         facing = "";
         idle = true;
         nextFire = 0;
-        fireRate = 250;
+
+        jumpSound = this.game.add.audio('jump');
+        jumpSound.volume = 0.5;
 
         playerVelocity = 200;
 
@@ -87,8 +77,8 @@ BasicGame.Game.prototype = {
         bullets.enableBody = true;
         bullets.physicsBodyType = Phaser.Physics.ARCADE;
 
-        for (var i = 0; i < 1000; i++) {
-            var b = bullets.create(0, 0, 'test_bullet');
+        for (var i = 0; i < 5000; i++) {
+            var b = bullets.create(0, 0, 'chicken_bullet');
             b.name = 'bullet' + i;
             b.exists = false;
             b.visible = false;
@@ -96,6 +86,40 @@ BasicGame.Game.prototype = {
             b.body.allowGravity = false
             b.events.onOutOfBounds.add(function(bullet) {bullet.kill()}, this);
         }
+
+        /* Weapons sounds */
+        pistolSound = this.game.add.audio('pistol_shoot');
+        pistolSound.volume = 0.5;
+
+        uziSound = this.game.add.audio('uzi_shoot');
+        uziSound.volume = 0.5;
+
+        // Gogo weapons !
+        weaponTypes = ["pistol", "uzi"];
+        weapons = {
+            "pistol": {
+                fireRate: 500,
+                sound: pistolSound
+            },
+            "uzi": {
+                fireRate: 100,
+                sound: uziSound
+            }
+        }
+
+        bulletType = weaponTypes[0];
+        fireRate = weapons[bulletType].fireRate;
+        currentWeaponSound = weapons[bulletType].sound;
+
+        // Weapon random changer
+        timer = this.time.create(false);
+        timer.loop(5000, function(){
+            bulletType = weaponTypes[Math.floor(Math.random()*weaponTypes.length)];
+            fireRate = weapons[bulletType].fireRate;
+            currentWeaponSound = weapons[bulletType].sound;
+            nextFire = 0;
+        }, this);
+        timer.start();
 
         // Camera settings
         this.camera.follow(player);
@@ -107,15 +131,16 @@ BasicGame.Game.prototype = {
     },
 
     createEnemy: function(x, y) {
-        var enemy_texture = this.add.bitmapData(32, 32);
+        var enemy_texture = this.add.bitmapData(32, 64);
         enemy_texture.ctx.beginPath();
-        enemy_texture.ctx.rect(0,0,32,32);
+        enemy_texture.ctx.rect(0,0,32,64);
         enemy_texture.ctx.fillStyle = '#27ae60';
         enemy_texture.ctx.fill();
 
         enemy = enemies.create(x, y, enemy_texture);
+        enemy.health = 3;
         enemy.body.gravity.y = 700;
-        enemy.velocity = -150;
+        enemy.velocity = -100;
         enemy.body.velocity.x = enemy.velocity;
         enemy.body.immovable = true;
 
@@ -128,7 +153,6 @@ BasicGame.Game.prototype = {
     update: function () {
         this.physics.arcade.collide(player, layer);
         this.physics.arcade.collide(player, outer);
-        this.physics.arcade.collide(player, layer_ladders);
 
         this.physics.arcade.collide(enemies, layer);
         this.physics.arcade.collide(enemies, outer, function(enemy, wall){
@@ -139,14 +163,24 @@ BasicGame.Game.prototype = {
             bullet.kill();
         });
 
+        this.physics.arcade.collide(bullets, outer, function(bullet) {
+            bullet.kill();
+        });
+
         this.physics.arcade.collide(player, enemies);
 
         this.physics.arcade.overlap(enemies, bullets, function(enemy, bullet){
+            enemy.damage(1);
             bullet.kill();
         });
 
         /* Enemy basic AI */
         enemies.forEach(function(enemy){
+            var distance = Math.sqrt(Math.pow((player.x - enemy.x), 2) + Math.pow((player.y - enemy.y), 2));
+            if(distance < 250 - this.rnd.integerInRange(-40, 40)) {
+                enemy.body.velocity = 0;
+            }
+            // enemy
         }.bind(this));
 
         /* MOVE MOVE */
@@ -156,6 +190,7 @@ BasicGame.Game.prototype = {
             if (facing != 'left') {
                 // player.animations.play('left');
                 facing = 'left';
+                player.scale.x = 1;
             }
             idle = false;
         } else if (cursors.right.isDown) {
@@ -164,27 +199,33 @@ BasicGame.Game.prototype = {
             if (facing != 'right') {
                 // player.animations.play('right');
                 facing = 'right';
+                player.scale.x = -1;
             }
             idle = false;
         } else {
+            /*
             // if (facing != 'idle') {
             if (!idle) {
                 // player.animations.stop();
 
                 if (facing == 'left') {
-                    player.frame = 0;
+                    // player.frame = 0;
+                    player.scale.x = 1;
                 } else {
-                    player.frame = 5;
+                    // player.frame = 5;
+                    player.scale.x = -1;
                 }
 
                 idle = true;
             }
+            */
         }
 
         /* JUMP JUMP ! */
         if (jumpButton.isDown && this.time.now > jumpTimer && player.body.velocity.y == 0) {
             player.body.velocity.y = -550;
             jumpTimer = this.time.now + 750;
+            jumpSound.play();
         }
 
         /* SHOOT ! */
@@ -192,14 +233,56 @@ BasicGame.Game.prototype = {
             if (this.time.time >= nextFire) {
                 bullet = bullets.getFirstExists(false);
                 if (bullet) {
+                    currentWeaponSound.play();
+                    var rangeCircle = this.game.add.graphics(0, 0);
+                    rangeCircle.x = 0;
+                    rangeCircle.y = 0;
+
+                    rangeCircle.lineStyle(0);
+                    rangeCircle.beginFill(0xFFFFFF);
+
                     if(facing == "right") {
-                        bullet.reset(player.x + 32, player.y + 16);
-                        bullet.body.velocity.x = 750;
+                        bullet.reset(player.x, player.y);
+                        rangeCircle.drawCircle(player.x,
+                                               player.y + 26,
+                                               20);
                     } else if (facing == "left") {
-                        bullet.reset(player.x - 32, player.y + 16);
-                        bullet.body.velocity.x = -750;
+                        bullet.reset(player.x - 25, player.y);
+                        rangeCircle.drawCircle(player.x,
+                                               player.y + 26,
+                                               20);
                     }
+
+                    rangeCircle.endFill();
+
+                    if(bulletType == "pistol") {
+                        var velocity = 750;
+                        bullet.angle = 0;
+                    } else if (bulletType == "uzi") {
+                        var velocity = 950;
+                        bullet.angle = 0;
+                        bullet.angle += this.rnd.integerInRange(-3, 3);
+                    }
+
+
+                    if(facing == "right") {
+                        this.physics.arcade.accelerationFromRotation(bullet.rotation,
+                                                                     velocity,
+                                                                     bullet.body.velocity);
+
+                        // bullet.body.velocity.x = velocity;
+                    } else if (facing == "left") {
+                        // bullet.body.velocity.x = -velocity;
+                        this.physics.arcade.accelerationFromRotation(bullet.rotation,
+                                                                     -velocity,
+                                                                     bullet.body.velocity);
+                    }
+
                     nextFire = this.time.time + fireRate;
+                    this.time.events.add(Phaser.Timer.SECOND * 0.05, function(){
+                        rangeCircle.clear();
+                    }, this);
+
                 }
             }
         }
